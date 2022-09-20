b5e216a1cdeb84532676af1b35d71fe5c163e091
Taylan Akyol <taylan.akyoll@gmail.com>
2022-09-20 17:09:37 +0300

    Initial commit

---
+++ Responsive_Website_Template/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Responsive_Webside_Template.Models;
+using System.Diagnostics;
+
+namespace Responsive_Webside_Template.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Responsive_Website_Template/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Responsive_Webside_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
